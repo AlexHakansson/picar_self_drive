0432b2500f4256f3de550125a4e71e3ee7acac8b
AlexHakansson <alex.hakansson9@gmail.com>
2022-09-17 12:24:11 -0700

    Add files via upload

--- routing.py
+++ routing.py
@@ -11,15 +11,15 @@ scan_angle_max = -90
 
 def main():
     while True:
-    scan_list = fc.scan_step(35)
+        scan_list = fc.scan_step(35)
     if not scan_list:
         continue
     tmp = scan_list[3:7]
     print(tmp)
     if tmp != [2,2,2,2]:
-    fc.turn_right(speed)
+        fc.turn_right(speed)
     else:
-    fc.forward(speed)
+        fc.forward(speed)
     
     
 if __name__ == "__main__":
